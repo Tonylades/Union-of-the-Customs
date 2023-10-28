dd222133ce7ff349b4d64d9e7c2bfbc4dc3b27a8
Tonylades <135992103+Tonylades@users.noreply.github.com>
2023-10-27 23:36:42 -0300

    Add files via upload

--- scripts/c999999991.lua
+++ scripts/c999999991.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_FIELD)
 	e0:SetCode(EFFECT_SPSUMMON_PROC)
 	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e0:SetRange(LOCATION_DECK+LOCATION_HAND_LOCATION_GRAVE)
+	e0:SetRange(LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE)
 	e0:SetOperation(s.spop)
 	c:RegisterEffect(e0)
     --(1)Immune
@@ -29,32 +29,57 @@ function s.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCode(EFFECT_UNRELEASABLE_SUM)
+	e3:SetCode(EFFECT_UNRELEASABLE_SUM+EFFECT_UNRELEASABLE_EFFECT)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
+    local e32=e3:Clone()
+	e32:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+	c:RegisterEffect(e32)
 	--(4)Set 1 "Dark Ruler Orders" Spell/Trap from the Deck
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1,{id,1})
+	e4:SetCountLimit(1)
 	e4:SetTarget(s.settg)
 	e4:SetOperation(s.setop)
 	c:RegisterEffect(e4)
+    --(5)Take infinite damage
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_DAMAGE)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_DESTROYED)
+	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5:SetTarget(s.damtg)
+	e5:SetOperation(s.damop)
+	c:RegisterEffect(e5)
+end
+--(1)
+function s.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
 end
 --(4)
 function s.setfilter(c)
-	return c:GetType()==TYPE_TRAP) or c:GetType()==TYPE_SPELL) and c:IsSSetable()
+	return c:IsSpellTrap() and c:IsSSetable()
 end
 function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
 end
 function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	local sg=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_DECK,0,nil)
-	if #sg==0 then return end
-	local ft=math.min(Duel.GetLocationCount(tp,LOCATION_SZONE),1)
-	local rg=aux.SelectUnselectGroup(sg,e,tp,1,ft,aux.dncheck,1,tp,HINTMSG_SET)
-	if #rg>0 then
-		Duel.SSet(tp,rg)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
 	end
+end
+--(5)
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(100000000)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,2000)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
 end
